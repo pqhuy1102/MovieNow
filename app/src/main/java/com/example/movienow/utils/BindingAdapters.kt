d28451e8ca610b6e9a0a9f1c5133dde55e8f3ba2
pqhuy0211 <pqhuy0211@gmail.com>
2022-11-21 17:29:12 +0700

    format date

--- app/src/main/java/com/example/movienow/utils/BindingAdapters.kt
+++ app/src/main/java/com/example/movienow/utils/BindingAdapters.kt
@@ -2,12 +2,16 @@ package com.example.movienow.utils
 
 import android.annotation.SuppressLint
 import android.os.Build
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import java.text.SimpleDateFormat
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.Date
 
 @BindingAdapter("loadImage")
 fun loadImage(view:ImageView, url:String?){
@@ -17,11 +21,17 @@ fun loadImage(view:ImageView, url:String?){
     }
 }
 
+@BindingAdapter("loadVote")
+fun loadVote(view: TextView, vote:Double){
+    view.text = "Votes: ${vote.toString()}"
+}
+
 @SuppressLint("SetTextI18n")
-@RequiresApi(Build.VERSION_CODES.O)
 @BindingAdapter("loadReleaseDate")
-fun loadReleaseDate(view: TextView, date:String){
-    val dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
-    val formattedDate = date.format(dateFormatter)
+fun loadReleaseDate(view: TextView, releaseDate:String){
+    val originalFormat = SimpleDateFormat("yyyy-MM-dd")
+    val formatter = SimpleDateFormat("dd-MM-yyyy")
+    val date:Date = originalFormat.parse(releaseDate)
+    val formattedDate = formatter.format(date)
     view.text = "Release Date: $formattedDate"
 }
